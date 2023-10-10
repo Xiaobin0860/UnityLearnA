9678cc0e929a6f391e659a37771b90aba84744b0
xl000 <l_xb@foxmail.com>
2023-10-10 21:40:41 +0800

    U1 - Player Control - L1.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20f);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
