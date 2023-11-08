8c6da18a2ef81d9e4628d8c2255174b11a738313
xl000 <l_xb@foxmail.com>
2023-11-09 00:14:11 +0800

    U3 - Sound and Effects - L3.3

--- Assets/Scripts/PlayerController3.cs
+++ Assets/Scripts/PlayerController3.cs
@@ -8,6 +8,7 @@ public class PlayerController3 : MonoBehaviour
     public float jumpForce = 10f;
     private bool isOnGround = true;
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public bool isGameOver = false;
 
     // Start is called before the first frame update
@@ -15,15 +16,17 @@ public class PlayerController3 : MonoBehaviour
     {
         Physics.gravity *= gravityModifier;
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !isGameOver)
         {
             isOnGround = false;
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
@@ -38,6 +41,8 @@ public class PlayerController3 : MonoBehaviour
         {
             isGameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
